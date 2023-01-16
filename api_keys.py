30a4574e2b6210ff582d828c9bad9b81c35a71ea
AmarRai99 <111756299+AmarRai99@users.noreply.github.com>
2023-01-16 19:52:33 +0000

    upload new

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "9aad71c8c50eec86b638305503b5c53d"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDKH_Sxbo6jZnKC-FMPCOltTr7ajzEPe4A"
